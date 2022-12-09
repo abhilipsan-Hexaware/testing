09fbd5792adb9a2b970beaf413a04ba91cc01564
abhilipsan-Hexaware <1000049191@hexaware.com>
2022-12-09 04:39:58 +0000

    1st commit

---
+++ dotnetwithmongo/Code/dotnetwithmongo.Api/Middleware/MappingFile.cs
@@ -0,0 +1,11 @@
+using dotnetwithmongo.BusinessEntities.Entities;
+using dotnetwithmongo.Contracts.DTO;
+using AutoMapper;
+
+public class MappingFile : Profile
+{
+    public MappingFile()
+    {
+        // Mapping variables
+    }
+}
